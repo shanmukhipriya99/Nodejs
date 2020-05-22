5ecf62e6a3e342cec462c745c7f5082474a290aa
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-05-22 16:19:11 +0530

    Using underscore module

---
+++ MoshCourse/Section-3/1-NodeDemo/index.js
@@ -0,0 +1,7 @@
+var _ = require('underscore');
+//core module
+//is a file or folder
+//is inside node_modules
+
+var result = _.contains([1, 2, 3], 4); // returns true or false
+console.log(result);
