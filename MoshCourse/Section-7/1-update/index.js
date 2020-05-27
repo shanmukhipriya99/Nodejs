65ab603047c913d87a7383f6cf15897f0e980721
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-05-27 17:23:17 +0530

    Update and remove

--- MoshCourse/Section-7/1-update/index.js
+++ MoshCourse/Section-7/1-update/index.js
@@ -15,15 +15,36 @@ const courseSchema = new mongoose.Schema({   //object
 
 const Course = mongoose.model('Course', courseSchema); 
 
+// async function updateCourse(id) {
+//     //Approach: Query first
+//     //findById(), modify, save
+//     const course = await Course.findById(id);
+//     if (!course) return;
+//     course.isPublished = true;
+//     course.author = 'Another person';
+//     const result = await course.save();
+//     console.log(result);
+// }
+
+// updateCourse('5ece4b3462e210fd0289cbd5');
+
 async function updateCourse(id) {
-    //Approach: Query first
-    //findById(), modify, save
-    const course = await Course.findById(id);
-    if (!course) return;
-    course.isPublished = true;
-    course.author = 'Another person';
-    const result = await course.save();
+    //Approach: Update first
+    const result = await Course.update({ _id: id}, { //to get the un-updated document, use: findByIdAndUpdate
+        $set: {                                      //to get the updaated document, use: findByIdAndUpdate and add a 3rd parameter
+            author: 'Shan',                             // after $set, { new: true }
+            isPublished: false                       // result => course
+        }
+    });
     console.log(result);
 }
 
 updateCourse('5ece4b3462e210fd0289cbd5');
+
+async function removeCourse(id) {
+    // const result = await Course.deleteOne({ _id: id }); //deletes the 1st document with that id (use deleteMany to delete multiple documents)
+    const course = await Course.findByIdAndRemove(id);
+    console.log(course);
+}
+
+// removeCourse('5ece4b3462e210fd0289cbd5');
