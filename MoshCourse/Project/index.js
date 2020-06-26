d55767e22f692bd183d34520a28a6dd89e630fcc
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-06-26 14:45:33 +0530

    Updated Section-9

--- MoshCourse/Project/index.js
+++ MoshCourse/Project/index.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
+const movies = require('./routes/movies');
+const rentals = require('./routes/rentals');
 const home = require('./routes/home');
 const express = require('express');
 const app = express();
@@ -12,6 +14,8 @@ mongoose.connect('mongodb://localhost/vidly')
 app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
+app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
 app.use('/', home);
 
 
---
+++ MoshCourse/Section-9/embedding.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/playground')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...', err));
+
+const authorSchema = new mongoose.Schema({
+  name: String,
+  bio: String,
+  website: String
+});
+
+const Author = mongoose.model('Author', authorSchema);
+
+const Course = mongoose.model('Course', new mongoose.Schema({
+  name: String,
+  author: authorSchema
+}));
+
+async function createCourse(name, author) {
+  const course = new Course({
+    name, 
+    author
+  }); 
+  
+  const result = await course.save();
+  console.log(result);
+}
+
+async function listCourses() { 
+  const courses = await Course.find();
+  console.log(courses);
+}
+
+async function updateAuthor(courseId) {
+  const course = await Course.update({ _id: courseId}, {  // use findById to update and save manually
+      $set: {                                            //use $unset to remove the object
+        'author.name': 'Jane Doe'
+      }
+  });
+}
+updateAuthor('5ef59d45aa0e2f8e04b82bc7');
+// createCourse('Node Course', new Author({ name: 'Mosh' }));   // ???
