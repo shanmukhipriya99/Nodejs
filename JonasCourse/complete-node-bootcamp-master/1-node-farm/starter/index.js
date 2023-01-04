bc0825b55052b29d0753cf9313261c3649e266f7
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-01-04 12:06:52 -0800

    fs: asynchronously

--- JonasCourse/complete-node-bootcamp-master/1-node-farm/starter/index.js
+++ JonasCourse/complete-node-bootcamp-master/1-node-farm/starter/index.js
@@ -3,9 +3,28 @@ const fs = require('fs');
 // const hello = 'Hello World';
 // console.log(hello);
 
+// blocking, synchronous way
 const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 console.log(textIn);
 
 const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on: ${Date.now()}`;
 fs.writeFileSync('./txt/output.txt', textOut);
 console.log('File written!');
+
+// Non-blocking, asynchronous way
+fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
+    console.log(data);
+});
+// Another way [CALLBACK HELL]
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+        console.log(data2);
+        fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+            console.log(data3);
+
+            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+                console.log('File has been written 🥳')
+            })
+        });
+    });
+});
