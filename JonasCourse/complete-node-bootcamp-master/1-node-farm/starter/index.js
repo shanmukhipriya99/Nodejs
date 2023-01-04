acbbb491268a3d07ed5af746cb58d741e661ab2f
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-01-04 11:43:16 -0800

    fs: reading and writing files

---
+++ JonasCourse/complete-node-bootcamp-master/1-node-farm/starter/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+// const hello = 'Hello World';
+// console.log(hello);
+
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+console.log(textIn);
+
+const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on: ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt', textOut);
+console.log('File written!');
