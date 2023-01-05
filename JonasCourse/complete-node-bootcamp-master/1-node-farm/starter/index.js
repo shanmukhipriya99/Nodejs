ce0fce2fec0305a64758280d23b6a982aa10c2d9
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-01-05 10:10:29 -0800

    http: creating a simple web server

--- JonasCourse/complete-node-bootcamp-master/1-node-farm/starter/index.js
+++ JonasCourse/complete-node-bootcamp-master/1-node-farm/starter/index.js
@@ -1,30 +1,47 @@
 const fs = require('fs');
+const http = require('http');
+
+/* --------- hello world --------- */
 
 // const hello = 'Hello World';
 // console.log(hello);
 
-// blocking, synchronous way
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(textIn);
+/* --------- fs: sync --------- */
 
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on: ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt', textOut);
-console.log('File written!');
+// blocking, synchronous way
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textIn);
+
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on: ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('File written!');
+
+/* --------- fs: async --------- */
 
 // Non-blocking, asynchronous way
-fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
-    console.log(data);
-});
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
+//     console.log(data);
+// });
 // Another way [CALLBACK HELL]
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-        console.log(data2);
-        fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
-            console.log(data3);
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         console.log(data2);
+//         fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//             console.log(data3);
 
-            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-                console.log('File has been written 🥳')
-            })
-        });
-    });
+//             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//                 console.log('File has been written 🥳')
+//             })
+//         });
+//     });
+// });
+
+/* --------- server --------- */
+const server = http.createServer((req, res) => {
+    console.log(req);
+    res.end('Hey, from the server!🤗')
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening on port 8000');
 });
