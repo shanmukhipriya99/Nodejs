6c13355dbb9a4d6cd23d2ba0569c450036f0e490
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-01-05 10:28:05 -0800

    url: Routing

--- JonasCourse/complete-node-bootcamp-master/1-node-farm/starter/index.js
+++ JonasCourse/complete-node-bootcamp-master/1-node-farm/starter/index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const http = require('http');
+const path = require('path');
+const url = require('url');
 
 /* --------- hello world --------- */
 
@@ -38,8 +40,20 @@ const http = require('http');
 
 /* --------- server --------- */
 const server = http.createServer((req, res) => {
-    console.log(req);
-    res.end('Hey, from the server!🤗')
+    // console.log(req.url);
+    const pathName = req.url;
+    if(pathName === '/' || pathName === '/overview') {
+        res.end('This is the OVERVIEW');
+    } else if (pathName === '/product') {
+        res.end('This is the PRODUCT');
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html' 
+        });
+        res.end('Page not found!');
+    }
+
+    // res.end('Hey, from the server!🤗')
 });
 
 server.listen(8000, '127.0.0.1', () => {
