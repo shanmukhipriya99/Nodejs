f59e8e694ab38c889fb06d07f55954195553f3a2
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-02-05 11:19:05 -0800

    api with promise

--- JonasCourse/complete-node-bootcamp-master/3-asynchronous-JS/starter/index.js
+++ JonasCourse/complete-node-bootcamp-master/3-asynchronous-JS/starter/index.js
@@ -1,18 +1,38 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
-const content = fs.readFile('./dog.txt', (err, data) => {
+// 1-callback method
+// fs.readFile('./dog.txt', (err, data) => {
+//   if (err) return console.log(`There was an error: ${err}`);
+//   console.log(`Breed: ${data}`);
+//   superagent
+//     .get(`https://dog.ceo/api/breed/${data}/images/random`)
+//     .end((err, res) => {
+//         if (err) return console.log(`There was an error: ${err}`);
+//       console.log(res.body.message);
+
+//       fs.writeFile('dog-img.txt', res.body.message, (err) => {
+//         if (err) return console.log(`There was an error: ${err}`);
+//         console.log('Random dog image saved!');
+//       });
+//     });
+// });
+
+// 2-Promises
+fs.readFile('./dog.txt', (err, data) => {
   if (err) return console.log(`There was an error: ${err}`);
   console.log(`Breed: ${data}`);
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-        if (err) return console.log(`There was an error: ${err}`);
+    .then((res) => {
       console.log(res.body.message);
 
       fs.writeFile('dog-img.txt', res.body.message, (err) => {
-        if (err) return console.log(`There was an error: ${err}`);
+        if (err) return console.log(err.message);
         console.log('Random dog image saved!');
       });
+    })
+    .catch((err) => {
+      console.log(err.message);
     });
 });
