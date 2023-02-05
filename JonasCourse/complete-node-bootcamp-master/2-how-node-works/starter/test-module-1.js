44cb8c131cd718511eb6f78aa6e88f7d6cf8c721
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-02-05 10:38:47 -0800

    module.exports

---
+++ JonasCourse/complete-node-bootcamp-master/2-how-node-works/starter/test-module-1.js
@@ -0,0 +1,18 @@
+// class Calculator {
+//   add(a, b) {
+//     return a + b;
+//   }
+//   multiply(a, b) {
+//     return a * b;
+//   }
+// }
+// module.exports = Calculator;
+
+module.exports = class {
+    add(a, b) {
+      return a + b;
+    }
+    multiply(a, b) {
+      return a * b;
+    }
+  };
