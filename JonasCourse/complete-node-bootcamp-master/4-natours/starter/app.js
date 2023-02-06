b318af3ef1dead99e299ee180390baec67d739b8
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-02-05 22:42:25 -0800

    URL parameters using req.params

--- JonasCourse/complete-node-bootcamp-master/4-natours/starter/app.js
+++ JonasCourse/complete-node-bootcamp-master/4-natours/starter/app.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
+app.use(express.json()); // middleware that handles the json input
+
 const port = 3000;
 
 // app.get('/', (req, res) => {
@@ -19,9 +21,48 @@ app.get('/api/v1/tours', (req, res) => {
     status: 'Success',
     results: tours.length,
     data: {
-      tours
+      tours,
     },
   });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  // console.log(req.params)
+  const tour = tours.find((el) => el.id === parseInt(req.params.id));
+  //   if (req.params.id > tours.length) {
+  if (!tour) {
+    return res.status(404).json({
+      status: 'Fail',
+      message: 'Invalid id',
+    });
+  }
+  res.status(200).json({
+    status: 'Success',
+    //   results: tours.length,
+    data: {
+      tour,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    './dev-data/data/tours-simple.json',
+    JSON.stringify(tours),
+    (err) => {
+      if (err) return console.log('Couldnt write the new tour');
+      res.status(201).json({
+        status: 'Success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 app.listen(port, () => {
