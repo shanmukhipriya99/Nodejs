d52593b83ec432c8834e05b0c6f381a90d43bfb7
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-02-05 19:31:13 -0800

    GET: /api/v1/tours

--- JonasCourse/complete-node-bootcamp-master/4-natours/starter/app.js
+++ JonasCourse/complete-node-bootcamp-master/4-natours/starter/app.js
@@ -1,14 +1,27 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Homepage💻', app: 'Natours' });
-});
+// app.get('/', (req, res) => {
+//   res.status(200).json({ message: 'Homepage💻', app: 'Natours' });
+// });
 
-app.post('/', (req, res) => {
-  res.status(200).send('This is a post');
+// app.post('/', (req, res) => {
+//   res.status(200).send('This is a post');
+// });
+
+const tours = JSON.parse(fs.readFileSync('./dev-data/data/tours-simple.json'));
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'Success',
+    results: tours.length,
+    data: {
+      tours
+    },
+  });
 });
 
 app.listen(port, () => {
