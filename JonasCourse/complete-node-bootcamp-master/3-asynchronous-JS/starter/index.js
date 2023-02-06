3210453139363342c3adc34703e0f3160ea92dda
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-02-05 16:24:50 -0800

    building promises

--- JonasCourse/complete-node-bootcamp-master/3-asynchronous-JS/starter/index.js
+++ JonasCourse/complete-node-bootcamp-master/3-asynchronous-JS/starter/index.js
@@ -19,20 +19,54 @@ const superagent = require('superagent');
 // });
 
 // 2-Promises
-fs.readFile('./dog.txt', (err, data) => {
-  if (err) return console.log(`There was an error: ${err}`);
-  console.log(`Breed: ${data}`);
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .then((res) => {
-      console.log(res.body.message);
+// fs.readFile('./dog.txt', (err, data) => {
+//   if (err) return console.log(`There was an error: ${err}`);
+//   console.log(`Breed: ${data}`);
+//   superagent
+//     .get(`https://dog.ceo/api/breed/${data}/images/random`)
+//     .then((res) => {
+//       console.log(res.body.message);
 
-      fs.writeFile('dog-img.txt', res.body.message, (err) => {
-        if (err) return console.log(err.message);
-        console.log('Random dog image saved!');
-      });
-    })
-    .catch((err) => {
-      console.log(err.message);
+//       fs.writeFile('dog-img.txt', res.body.message, (err) => {
+//         if (err) return console.log(err.message);
+//         console.log('Random dog image saved!');
+//       });
+//     })
+//     .catch((err) => {
+//       console.log(err.message);
+//     });
+// });
+
+// 3-Building Promises (Retrun a promise everytime to create a chain of then())
+const readFileProm = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('Couldnt find the file');
+      resolve(data);
     });
-});
+  });
+};
+const writeFileProm = (imgUrl) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile('./dog-img.txt', imgUrl, (err) => {
+      if (err) reject('Couldnt write to the file🥶');
+      resolve('Success🥳');
+    });
+  });
+};
+
+readFileProm('./dog.txt')
+  .then((data) => {
+    console.log(`Breed: ${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    console.log(res.body.message);
+    return writeFileProm(res.body.message);
+  })
+  .then(() => {
+    console.log('Random dog image saved!');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
