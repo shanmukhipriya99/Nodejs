37981c4e6638de8995821fbb679cc039929618c0
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-02-07 22:08:38 -0800

    prod script

--- JonasCourse/complete-node-bootcamp-master/4-natours/starter/app.js
+++ JonasCourse/complete-node-bootcamp-master/4-natours/starter/app.js
@@ -5,6 +5,7 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 // Middlewares
+// console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
