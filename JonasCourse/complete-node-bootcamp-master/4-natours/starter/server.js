de2000fffeeddea2649f1ba363c3f1dfed885f77
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-02-09 17:48:23 -0800

    connecting to mongoose

--- JonasCourse/complete-node-bootcamp-master/4-natours/starter/server.js
+++ JonasCourse/complete-node-bootcamp-master/4-natours/starter/server.js
@@ -1,7 +1,23 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 // console.log(process.env);
+const db = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    // console.log(connection.connections);
+    console.log('DB connected successfully');
+  });
 
 // Start Server
 const port = 3000 || process.env.PORT;
