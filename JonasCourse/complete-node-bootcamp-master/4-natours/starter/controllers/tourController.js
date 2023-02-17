48a80544c41d34c7d8d2c24239e5ea02e48d6fb7
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-02-17 14:42:20 -0800

    advanced filtering

--- JonasCourse/complete-node-bootcamp-master/4-natours/starter/controllers/tourController.js
+++ JonasCourse/complete-node-bootcamp-master/4-natours/starter/controllers/tourController.js
@@ -28,20 +28,30 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
+    console.log(req.query);
     // BUILD the query
+    // 1. Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
-    console.log(req.query, queryObj);
-    const query = Tour.find(queryObj); // to be able to perform advanced filtering on the obj
+
+    // 2. Advanced Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    // { difficulty: easy, duration: { $gte: 5 } } => What MongoDB requires...
+    // { duration: { gte: '5' }, difficulty: 'easy' } => What is parsed...
+    // const query = Tour.find(queryObj); // to be able to perform advanced filtering on the obj
+    const query = Tour.find(JSON.parse(queryStr));
+
+    // EXECUTE the query
+    const tours = await query;
+
     // const query = Tour.find()
     //   .where('duration')
     //   .equals(5)
     //   .where('difficulty')
     //   .equals('easy');
-
-    // EXECUTE the query
-    const tours = await query;
 
     // SEND response
     res.status(200).json({
