6d511a1a6ca590b09cc5cb542621025a357cfaeb
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-02-17 18:41:38 -0800

    limiting fields

--- JonasCourse/complete-node-bootcamp-master/4-natours/starter/controllers/tourController.js
+++ JonasCourse/complete-node-bootcamp-master/4-natours/starter/controllers/tourController.js
@@ -52,6 +52,14 @@ exports.getAllTours = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
+    // 3. Field Limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
     // EXECUTE the query
     const tours = await query;
 
