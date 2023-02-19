4d2cd7b545f01a16dc1ae76cae0a6321c677b77a
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-02-19 12:22:53 -0800

    basic user schema

---
+++ JonasCourse/complete-node-bootcamp-master/4-natours/starter/models/userModel.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+    trim: true,
+    maxLength: [40, 'Name must not have more than 40 characters'],
+    minLength: [10, 'Name must not have less than 10 characters'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    lowercase: true, // not a validator, just converts it to lowercase
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    minLength: [8, 'Password must have less atleast 8 characters'],
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Password is required'],
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
