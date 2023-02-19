79418eb6cdcb6d5e390c94d61d8b5af7655f4552
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-02-18 23:59:31 -0800

    handling invalid db ids

--- JonasCourse/complete-node-bootcamp-master/4-natours/starter/controllers/errorController.js
+++ JonasCourse/complete-node-bootcamp-master/4-natours/starter/controllers/errorController.js
@@ -1,3 +1,10 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
 const sendErrDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -32,6 +39,9 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrProd(err, res);
+    let error = { ...err };
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+
+    sendErrProd(error, res);
   }
 };
