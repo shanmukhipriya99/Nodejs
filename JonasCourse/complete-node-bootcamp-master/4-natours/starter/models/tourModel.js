edd390bee9b2937c31322591918ccc2d90d77b3d
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-02-18 18:39:50 -0800

    Query middleware

--- JonasCourse/complete-node-bootcamp-master/4-natours/starter/models/tourModel.js
+++ JonasCourse/complete-node-bootcamp-master/4-natours/starter/models/tourModel.js
@@ -56,6 +56,10 @@ const tourSchema = new mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -67,7 +71,7 @@ tourSchema.virtual('durationWeeks').get(function () {
   // use a regular function to get access to 'this' keyword
   return this.duration / 7;
 });
-// Document middleware: runs before .save() & .create()
+// Document middleware: runs before/after .save() & .create()
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
   next();
@@ -80,6 +84,20 @@ tourSchema.pre('save', function (next) {
 //   console.log(doc);
 //   next();
 // });
+
+// Query Middleware: runs before/after executing a query
+tourSchema.pre(/^find/, function (next) {
+  // tourSchema.pre('find', function (next) {
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took: ${Date.now() - this.start} milliseconds!`);
+  console.log(docs);
+  next();
+});
 
 const Tour = mongoose.model('Tour', tourSchema);
 
