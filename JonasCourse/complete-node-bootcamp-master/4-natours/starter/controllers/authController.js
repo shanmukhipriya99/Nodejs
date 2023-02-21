751b47627612565cd82abcbbd4f47d1d1ccd28f0
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-02-20 23:31:09 -0800

    signing up users

--- JonasCourse/complete-node-bootcamp-master/4-natours/starter/controllers/authController.js
+++ JonasCourse/complete-node-bootcamp-master/4-natours/starter/controllers/authController.js
@@ -1,11 +1,23 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res) => {
-  const newUser = await User.create(req.body);
+  // has a flaw, saves req.body giving user control to register as with an admin role as well
+  //   const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES,
+  });
 
   res.status(201).json({
     staus: 'success',
+    token,
     data: {
       user: newUser,
     },
