39098c9033fec0d2b6d5a17029bff346cb520c20
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-03-20 12:58:09 -0700

    body limit

--- JonasCourse/complete-node-bootcamp-master/4-natours/starter/app.js
+++ JonasCourse/complete-node-bootcamp-master/4-natours/starter/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -9,6 +10,7 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 // Global Middlewares
+app.use(helmet()); // Set security HTTP headers
 // console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -19,9 +21,13 @@ const limiter = rateLimit({
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour',
 });
-app.use('/api', limiter);
+app.use('/api', limiter); // Limit requests from the same IP
 
-app.use(express.json()); // middleware that handles the json input
+app.use(
+  express.json({
+    limit: '10kb',
+  })
+); // middleware that handles the json input
 app.use(express.static('./public'));
 // middleware functions that apply to every API call if positioned before the route handler
 app.use((req, res, next) => {
