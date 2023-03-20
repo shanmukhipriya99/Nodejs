040e32c677a5fe49fd9d73f521e6d2247a714a33
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-03-20 12:50:11 -0700

    express-rate-limit

--- JonasCourse/complete-node-bootcamp-master/4-natours/starter/app.js
+++ JonasCourse/complete-node-bootcamp-master/4-natours/starter/app.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
-// Middlewares
+// Global Middlewares
 // console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour',
+});
+app.use('/api', limiter);
+
 app.use(express.json()); // middleware that handles the json input
 app.use(express.static('./public'));
 // middleware functions that apply to every API call if positioned before the route handler
