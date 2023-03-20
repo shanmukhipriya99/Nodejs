99bda0a2c8d5cefcf9ff2e5f9f3da5937d5ed8df
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-03-19 21:02:10 -0700

    update profile

--- JonasCourse/complete-node-bootcamp-master/4-natours/starter/controllers/userController.js
+++ JonasCourse/complete-node-bootcamp-master/4-natours/starter/controllers/userController.js
@@ -1,9 +1,17 @@
 const User = require('../models/userModel');
+const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+};
+
 // Route Handlers
 exports.getAllUsers = catchAsync(async (req, res) => {
   const users = await User.find();
-
   // SEND response
   res.status(200).json({
     status: 'Success',
@@ -11,6 +19,26 @@ exports.getAllUsers = catchAsync(async (req, res) => {
     results: users.length,
     data: {
       users,
+    },
+  });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create error if user tries to POST password data
+  if (req.body.password || req.body.passwordConfirm) {
+    next(new AppError('Cannot update password here', 400));
+  }
+  // 2) Filtered out unwanted field names that are not allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  // 3) Update user document
+  const updateUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({
+    status: 'Success',
+    data: {
+      user: updateUser,
     },
   });
 });
