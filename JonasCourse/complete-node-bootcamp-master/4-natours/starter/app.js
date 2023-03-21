6e39d6a2f0005df4de08a80cb04fbaa5f98d1b95
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-03-21 16:21:15 -0700

    creating and getting reviews

--- JonasCourse/complete-node-bootcamp-master/4-natours/starter/app.js
+++ JonasCourse/complete-node-bootcamp-master/4-natours/starter/app.js
@@ -10,6 +10,7 @@ const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 // Global Middlewares
@@ -65,6 +66,7 @@ app.use((req, res, next) => {
 // List of APIs/Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 // handling unhandled routes
 app.all('*', (req, res, next) => {
   // res.status(404).json({
---
+++ JonasCourse/complete-node-bootcamp-master/4-natours/starter/routes/reviewRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const router = express.Router();
+const reviewController = require('../controllers/reviewController');
+const authController = require('../controllers/authController');
+
+router
+  .route('/')
+  .get(reviewController.getAllReviews)
+  .post(
+    authController.protect,
+    authController.restrictTo('user'),
+    reviewController.createReview
+  );
+
+router.get('/:id', reviewController.getReview);
+
+module.exports = router;
