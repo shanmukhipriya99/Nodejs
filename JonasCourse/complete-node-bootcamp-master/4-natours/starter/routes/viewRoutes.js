ae5d19096b8f0c8fb543815d8221f9daa84cdc94
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-03-23 12:44:19 -0700

    project structure

---
+++ JonasCourse/complete-node-bootcamp-master/4-natours/starter/routes/viewRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const viewsController = require('../controllers/viewsController');
+
+const router = express.Router();
+
+// router.get('/', (req, res) => {
+//   res.status(200).render('base', {
+//     tour: 'The Forest Hiker',
+//     user: 'Sandy',
+//   });
+// });
+
+router.get('/', viewsController.getOverview);
+
+router.get('/tour', viewsController.getTour);
+
+module.exports = router;
