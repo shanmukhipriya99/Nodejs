ea167b3456dbe235df49705a33a0f40d7ac15690
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-03-22 21:46:37 -0700

    geospatial aggregation: calculating distances

--- JonasCourse/complete-node-bootcamp-master/4-natours/starter/controllers/tourController.js
+++ JonasCourse/complete-node-bootcamp-master/4-natours/starter/controllers/tourController.js
@@ -233,3 +233,42 @@ exports.getToursWithin = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+exports.getDistances = catchAsync(async (req, res, next) => {
+  const { latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+  const multiplier = unit === 'mi' ? 0.000621371 : 0.001;
+  if (!lat || !lng) {
+    next(
+      new AppError('Please provide lat and long in the format: lat,long', 400)
+    );
+  }
+  const distances = await Tour.aggregate([
+    {
+      // $geoNear has to be the first in the pipeline
+      // $geoNear has to have atleast one geospatial index
+      // tourSchema.index({ startLocation: '2dsphere})
+      $geoNear: {
+        near: {
+          type: 'Point',
+          coordintes: [lng * 1, lat * 1],
+        },
+        distanceField: 'distance',
+        distanceMultiplier: multiplier,
+      },
+    },
+    {
+      $project: {
+        distance: 1,
+        name: 1,
+      },
+    },
+  ]);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      data: distances,
+    },
+  });
+});
