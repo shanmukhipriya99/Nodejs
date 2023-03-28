4d08e09f4cc17a9ffcbad026416851cdcaa417e1
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-03-28 16:22:55 -0700

    update password

--- JonasCourse/complete-node-bootcamp-master/4-natours/starter/public/js/index.js
+++ JonasCourse/complete-node-bootcamp-master/4-natours/starter/public/js/index.js
@@ -9,6 +9,7 @@ const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector('.form-user-password');
 
 // DELEGATION
 if (mapBox) {
@@ -26,9 +27,28 @@ if (loginForm) {
 
 if (logOutBtn) logOutBtn.addEventListener('click', logout);
 
-if (userDataForm) userDataForm.addEventListener('submit', e => {
+if (userDataForm)
+  userDataForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
-    
-})
+    updateSettings({ name, email }, 'data');
+  });
+
+if (userPasswordForm)
+  userPasswordForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    document.querySelector('btn--save-password').textContent = 'Updating...';
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+    await updateSettings(
+      { passwordCurrent, password, passwordConfirm },
+      'password'
+    );
+
+    document.querySelector('btn--save-password').textContent = 'Save Password';
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
+  });
