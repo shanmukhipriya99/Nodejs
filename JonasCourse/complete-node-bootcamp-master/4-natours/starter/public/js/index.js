81aa5a47301ce9d07f22e00a6d41ddf229bfd350
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-03-28 16:12:52 -0700

    updating with API

--- JonasCourse/complete-node-bootcamp-master/4-natours/starter/public/js/index.js
+++ JonasCourse/complete-node-bootcamp-master/4-natours/starter/public/js/index.js
@@ -1,13 +1,14 @@
 /* eslint-disable */
 import '@babel/polyfill';
 import { displayMap } from './mapbox';
-import { login } from './login';
-import { logout } from '../../controllers/authController';
+import { login, logout } from './login';
+import { updateSettings } from './updateSettings';
 
 // DOM Elements
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
 
 // DELEGATION
 if (mapBox) {
@@ -24,3 +25,10 @@ if (loginForm) {
 }
 
 if (logOutBtn) logOutBtn.addEventListener('click', logout);
+
+if (userDataForm) userDataForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    
+})
