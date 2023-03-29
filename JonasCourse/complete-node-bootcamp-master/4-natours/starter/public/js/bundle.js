2e6e46f9a8cd7076607b85b38f23a8bc7a60037f
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-03-29 09:59:17 -0700

    image upload from form

--- JonasCourse/complete-node-bootcamp-master/4-natours/starter/public/js/bundle.js
+++ JonasCourse/complete-node-bootcamp-master/4-natours/starter/public/js/bundle.js
@@ -12132,6 +12132,9 @@ var updateSettings = /*#__PURE__*/function () {
           res = _context.sent;
           if (res.data.status === 'success') {
             (0, _alert.showAlert)('success', "".concat(type.toUpperCase(), " updated successfully!"));
+            window.setTimeout(function () {
+              location.reload();
+            }, 1000);
           }
           _context.next = 11;
           break;
@@ -12315,12 +12318,13 @@ if (loginForm) {
 if (logOutBtn) logOutBtn.addEventListener('click', _login.logout);
 if (userDataForm) userDataForm.addEventListener('submit', function (e) {
   e.preventDefault();
-  var name = document.getElementById('name').value;
-  var email = document.getElementById('email').value;
-  (0, _updateSettings.updateSettings)({
-    name: name,
-    email: email
-  }, 'data');
+  var form = new FormData();
+  form.append('name', document.getElementById('name').value);
+  form.append('email', document.getElementById('email').value);
+  form.append('photo', document.getElementById('photo').files[0]);
+  // const name = document.getElementById('name').value;
+  // const email = document.getElementById('email').value;
+  (0, _updateSettings.updateSettings)(form, 'data');
 });
 if (userPasswordForm) userPasswordForm.addEventListener('submit', /*#__PURE__*/function () {
   var _ref = _asyncToGenerator( /*#__PURE__*/_regeneratorRuntime().mark(function _callee(e) {
