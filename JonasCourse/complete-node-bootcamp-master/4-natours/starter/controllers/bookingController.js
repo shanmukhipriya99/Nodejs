739fe6c8738106f7aa63fe259ef71ca7f12aa365
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-04-03 16:18:48 -0700

    Completed Bookings API

--- JonasCourse/complete-node-bootcamp-master/4-natours/starter/controllers/bookingController.js
+++ JonasCourse/complete-node-bootcamp-master/4-natours/starter/controllers/bookingController.js
@@ -2,7 +2,6 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const catchAsync = require('../utils/catchAsync');
 const Tour = require('../models/tourModel');
 const Booking = require('../models/bookingModel');
-const AppError = require('../utils/appError');
 const factory = require('./handleFactory');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
@@ -46,3 +45,9 @@ exports.createBookingCheckout = catchAsync(async (req, res, next) => {
   await Booking.create({ tour, user, price });
   res.redirect(req.originalUrl.split('?')[0]);
 });
+
+exports.createBooking = factory.createOne(Booking);
+exports.getBooking = factory.getOne(Booking);
+exports.getAllBookings = factory.getAll(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
