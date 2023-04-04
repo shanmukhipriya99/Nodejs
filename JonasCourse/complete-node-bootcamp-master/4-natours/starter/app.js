631360fc66f0d886ce2221a3b4b149f8800a0bcb
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-04-03 21:21:08 -0700

    compression middleware

--- JonasCourse/complete-node-bootcamp-master/4-natours/starter/app.js
+++ JonasCourse/complete-node-bootcamp-master/4-natours/starter/app.js
@@ -7,6 +7,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -132,6 +133,8 @@ app.use(
     ],
   })
 );
+
+app.use(compression());
 
 // middleware functions that apply to every API call if positioned before the route handler
 app.use((req, res, next) => {
