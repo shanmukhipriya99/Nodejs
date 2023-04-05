e69ac607a18e1afb1fd486815653289d0fea6a3c
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-04-04 22:31:16 -0700

    CORS

--- JonasCourse/complete-node-bootcamp-master/4-natours/starter/app.js
+++ JonasCourse/complete-node-bootcamp-master/4-natours/starter/app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -23,6 +24,10 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // Global Middlewares
+
+// Implement CORS
+app.use(cors());
+app.options('*', cors());
 // Serving static files
 // app.use(express.static('./public'));
 app.use(express.static(path.join(__dirname, 'public')));
