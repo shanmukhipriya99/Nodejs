99f0691193d26a0bcd773fcd5fb39d814a55a3ed
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-04-06 19:44:30 -0700

    default profile photo

--- JonasCourse/complete-node-bootcamp-master/4-natours/starter/models/userModel.js
+++ JonasCourse/complete-node-bootcamp-master/4-natours/starter/models/userModel.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema({
   },
   photo: {
     type: String,
-    default: `${__dirname}/../../public/img/users/default.jpg`,
+    default: 'default.jpeg',
   },
   role: {
     type: String,
