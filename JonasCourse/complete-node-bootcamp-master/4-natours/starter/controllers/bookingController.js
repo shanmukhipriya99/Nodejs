aa143f1a30f15705fe38978999ef2e66fb83c50a
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-04-06 18:46:17 -0700

    client_reference_id

--- JonasCourse/complete-node-bootcamp-master/4-natours/starter/controllers/bookingController.js
+++ JonasCourse/complete-node-bootcamp-master/4-natours/starter/controllers/bookingController.js
@@ -18,7 +18,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     success_url: `${req.protocol}://${req.get('host')}/my-tours`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
-    client_reference_id: req.params.tourID,
+    client_reference_id: req.params.tourId,
     line_items: [
       {
         price_data: {
